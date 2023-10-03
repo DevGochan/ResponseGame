227384398049d9a5be46d5164b76dedf8c2ad1d2
Devgochan <devgochan@gmail.com>
2023-10-03 12:41:21 +0900

    First Commit

---
+++ app/src/main/java/com/ghochobo/study/ResponseTime.kt
@@ -0,0 +1,11 @@
+package com.ghochobo.study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResponseTime : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_response_time)
+    }
+}
